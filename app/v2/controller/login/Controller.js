a877a66408c2d7c43e0253ad1dba57212a06d890
manirathinam <manirathinam@kpost.in>
2019-11-02 18:35:37 +0530

    Changes

--- app/v2/controller/login/Controller.js
+++ app/v2/controller/login/Controller.js
@@ -1,34 +1,75 @@
 var db = require('../../config/db/mysqldb');
 
-// exports.sampleget = function (req, res) {
-//     res.send('Get method calling...')
-// }
 exports.post = function (req, res) {
-    var dbcon=db.connect();
-    const data = req.body;
-    var obj;
+  var dbcon=db.connect();
+  // const data = req.body;
+  // var obj;
+  let sql = "CALL login('" + req.body.mailId + "','" + req.body.password + "')";
+    console.log(sql)
     
-    var sql = "select * from signup where mailid='"+data.mailId+"' and password='"+data.password+"'";
-    dbcon.query(sql, function (err, result, fields) {
-      if (err)
-      {
-        console.log("Something Error");
-        return false;
-      } 
-      var datalength = result.length;
-      if(datalength > 0)
-      {
-        obj={
-            "status":"success",
-            "value":result
+    dbcon.query(sql, function(err, rows, fields) {
+        if (err == null) {
+            console.log(rows[0])
+            var datalength = rows.length;
+            console.log(datalength);
+            const vals = rows[0];
+            if(datalength > 0)
+            {
+
+            
+            if(vals[0].ACTIVESTATUS=='yes')
+            {
+              var obj = {
+                "message": "success",
+                "value":vals[0]
             }
-      }
-      else{
-        obj={
-            "status":"fail",
-            "value":result
+            res.send(obj)
             }
-      }
-      res.send(obj);
-    });
+            else{
+              var obj = {
+                "message": "fail",
+                "errmsg":"Your Account not still Active",
+                "value":"notactive"
+            }
+            res.send(obj)
+            }
+          }
+         }
+         else {
+            res.send({
+                "message ": "failure",
+                "value": err.sqlMessage,
+            })
+        }
+    })
 }
+
+// exports.post = function (req, res) {
+//     var dbcon=db.connect();
+//     const data = req.body;
+//     var obj;
+    
+//     var sql = "select * from signup where mailid='"+data.mailId+"' and password='"+data.password+"'";
+//     dbcon.query(sql, function (err, result, fields) {
+//       if (err)
+//       {
+//         console.log("Something Error");
+//         return false;
+//       } 
+//       var datalength = result.length;
+//       if(datalength > 0)
+//       {
+//         obj={
+//             "status":"success",
+//             "value":result
+//             }
+//       }
+//       else{
+//         obj={
+//             "status":"fail",
+//             "value":result
+//             }
+//       }
+//       res.send(obj);
+//     });
+// }
--- app/v2/controller/signup/Controller.js
+++ app/v2/controller/signup/Controller.js
@@ -3,47 +3,71 @@ var db = require('../../config/db/mysqldb');
 exports.sampleget = function (req, res) {
     res.send('Get method calling...')
 }
+
 exports.samplepost = function (req, res) {
-    var dbcon=db.connect();
-    const data = req.body;
-    var obj;
-    var errormsg;
-    var sql = "INSERT INTO signup (username, mailid, mobile, password) VALUES ('"+data.userName+"','"+data.mailId+"','"+data.mobile+"','"+data.password+"')";
-    dbcon.query(sql).on('result', function (result) {
-        if(result.affectedRows==1)
-        {
-            obj={
-                "status":"success",
-                "errorMessage":"Profile Created Successfully"
+    var dbcon = db.connect();
+   
+    let sql = "CALL signup('" + req.body.firstName + "','" + req.body.lastName + "','" + req.body.mailId + "','" + req.body.mobile + "','" + req.body.password + "')";
+    //console.log(sql)
+    
+    dbcon.query(sql, function(err, rows, fields) {
+        if (err == null) {
+            console.log(rows)
+            var obj = {
+                "message": "success"
             }
+            res.send(obj)
+         }
+         else {
+            res.send({
+                "message ": "failure",
+                "value": err.sqlMessage,
+            })
         }
-    }).on('error', function (err) {
-        //console.log(err);
-        if(err.code=='ER_DUP_ENTRY')
-        {
-          //  console.log(err.sqlMessage)
-            let errorindex  = err.sqlMessage.split("for key");
-            let errorindex1 = errorindex[1].split("UNIQUE");
-            let errorcolumn = errorindex1[0].split("'")
+    })
+}
 
-            console.log(errorindex1);
-            console.log(errorcolumn[1]);
+// exports.samplepost = function (req, res) {
+//     var dbcon=db.connect();
+//     const data = req.body;
+//     var obj;
+//     var errormsg;
+//     var sql = "INSERT INTO signup (username, mailid, mobile, password) VALUES ('"+data.userName+"','"+data.mailId+"','"+data.mobile+"','"+data.password+"')";
+//     dbcon.query(sql).on('result', function (result) {
+//         if(result.affectedRows==1)
+//         {
+//             obj={
+//                 "status":"success",
+//                 "errorMessage":"Profile Created Successfully"
+//             }
+//         }
+//     }).on('error', function (err) {
+//         //console.log(err);
+//         if(err.code=='ER_DUP_ENTRY')
+//         {
+//           //  console.log(err.sqlMessage)
+//             let errorindex  = err.sqlMessage.split("for key");
+//             let errorindex1 = errorindex[1].split("UNIQUE");
+//             let errorcolumn = errorindex1[0].split("'")
+
+//             console.log(errorindex1);
+//             console.log(errorcolumn[1]);
 
 
-            errormsg=errorcolumn[1] + "is Exit";
-        }
-        else{
-            errormsg = err.sqlMessage
-        }
-        obj={
-            "status":"fail",
-            "errorMessage":errormsg
-        }
-    }).on('end', function (data) {
-        //Result return to request respons 
-        res.send(obj);
-    });
+//             errormsg=errorcolumn[1] + "is Exit";
+//         }
+//         else{
+//             errormsg = err.sqlMessage
+//         }
+//         obj={
+//             "status":"fail",
+//             "errorMessage":errormsg
+//         }
+//     }).on('end', function (data) {
+//         //Result return to request respons 
+//         res.send(obj);
+//     });
     
     
    
-}
+// }
