1fdbe6ffb754a819cdcd05f75b5f4420d98d32f1
sathish <sathishkathirvel123@gmail.com>
2019-12-05 19:49:59 +0530

    sathish

--- app/v2/controller/event/Controller.js
+++ app/v2/controller/event/Controller.js
@@ -4,45 +4,43 @@ exports.sampleget = function (req, res) {
     res.send('Get method calling...')
 }
 exports.samplepost = function (req, res) {
-    
+
     res.send('POST method calling...')
 }
 exports.paymentdetails = function (req, res) {
-    
+
     res.send('paymentdetails method calling...')
 }
 
 exports.addEvents = function (req, res) {
-    var dbcon=db.connect();
+    var dbcon = db.connect();
     const data = req.body;
     var obj;
     var errormsg;
 
-    var sql = "INSERT INTO events (eventtype, subeventtype,eventname, price,prizeamount, totaltokens,starttocken,expiredate)" + 
-    "VALUES ('"+data.eventtype+"','"+data.subeventtype+"','"+data.eventname+"','"+data.price+"','"+data.prizeamount+"','"+data.totaltokens+"','"+data.starttocken+"','"+data.expiredate+"')";
+    var sql = "INSERT INTO events (eventtype, subeventtype,eventname, price,prizeamount, totaltokens,starttocken,expiredate)" +
+        "VALUES ('" + data.eventtype + "','" + data.subeventtype + "','" + data.eventname + "','" + data.price + "','" + data.prizeamount + "','" + data.totaltokens + "','" + data.starttocken + "','" + data.expiredate + "')";
     dbcon.query(sql).on('result', function (result) {
-        if(result.affectedRows==1)
-        {
-            obj={
-                "status":"success",
-                "errorMessage":"Events Add success"
+        if (result.affectedRows == 1) {
+            obj = {
+                "status": "success",
+                "errorMessage": "Events Add success"
             }
         }
     }).on('error', function (err) {
         //console.log(err);
-        if(err.code=='ER_DUP_ENTRY')
-        {
-            let errorindex  = err.sqlMessage.split("for key");
+        if (err.code == 'ER_DUP_ENTRY') {
+            let errorindex = err.sqlMessage.split("for key");
             let errorindex1 = errorindex[1].split("UNIQUE");
             let errorcolumn = errorindex1[0].split("'")
-            errormsg=errorcolumn[1] + "is Exit";
+            errormsg = errorcolumn[1] + "is Exit";
         }
-        else{
+        else {
             errormsg = err.sqlMessage
         }
-        obj={
-            "status":"fail",
-            "errorMessage":errormsg
+        obj = {
+            "status": "fail",
+            "errorMessage": errormsg
         }
     }).on('end', function (data) {
         //Result return to request respons 
@@ -51,28 +49,27 @@ exports.addEvents = function (req, res) {
 }
 
 exports.updateEvents = function (req, res) {
-  var dbcon=db.connect();
+    var dbcon = db.connect();
     const data = req.body;
     var obj;
     var errormsg;
     // var sql = "INSERT INTO events (eventtype, subeventtype,eventname, price, totaltokens,expiredate)" + 
     // "VALUES ('"+data.eventtype+"','"+data.subeventtype+"','"+data.eventname+"','"+data.price+"','"+data.totaltokens+"','"+data.expiredate+"')";
 
-    var sql = "UPDATE events SET eventtype='"+data.eventtype+"',subeventtype='"+data.subeventtype+"',eventname='"+data.eventname+"',price='"+data.price+"',totaltokens='"+data.totaltokens+"',expiredate='"+data.expiredate+"' WHERE id='"+data.id+"'"; 
-console.log(sql);
+    var sql = "UPDATE events SET eventtype='" + data.eventtype + "',subeventtype='" + data.subeventtype + "',eventname='" + data.eventname + "',price='" + data.price + "',totaltokens='" + data.totaltokens + "',expiredate='" + data.expiredate + "' WHERE id='" + data.id + "'";
+    console.log(sql);
     dbcon.query(sql).on('result', function (result) {
-        if(result.affectedRows==1)
-        {
-            obj={
-                "status":"success",
-                "errorMessage":"Events Updated Successfully"
+        if (result.affectedRows == 1) {
+            obj = {
+                "status": "success",
+                "errorMessage": "Events Updated Successfully"
             }
         }
     }).on('error', function (err) {
-            errormsg = err.sqlMessage
-        obj={
-            "status":"fail",
-            "errorMessage":errormsg
+        errormsg = err.sqlMessage
+        obj = {
+            "status": "fail",
+            "errorMessage": errormsg
         }
     }).on('end', function (data) {
         //Result return to request respons 
@@ -81,78 +78,74 @@ console.log(sql);
 }
 
 exports.getEvents = function (req, res) {
-    var dbcon=db.connect();
+    var dbcon = db.connect();
     const data = req.body;
     var obj;
-    
+
     // var sql = "select * from events where email='"+data.mailId+"'"; expiredate
-    var sql = "select * from events WHERE EXPIREDATE >='"+data.expiredate+"' and EVENTTYPE='"+data.eventtype+"'";
+    var sql = "select * from events WHERE EXPIREDATE >='" + data.expiredate + "' and EVENTTYPE='" + data.eventtype + "'";
     console.log(sql);
     dbcon.query(sql, function (err, result, fields) {
         console.log(result);
-      if (err)
-      {
-        console.log("Something Error");
-        console.log(err);
-        return false;
-      } 
-      var datalength = result.length;
-      if(datalength > 0)
-      {
-        obj={
-            "status":"success",
-            "value":result
+        if (err) {
+            console.log("Something Error");
+            console.log(err);
+            return false;
+        }
+        var datalength = result.length;
+        if (datalength > 0) {
+            obj = {
+                "status": "success",
+                "value": result
             }
-      }
-      else{
-        obj={
-            "status":"fail",
-            "value":result
+        }
+        else {
+            obj = {
+                "status": "fail",
+                "value": result
             }
-      }
-      res.send(obj);
+        }
+        res.send(obj);
     });
 }
 
 exports.alluserbookedEvents = function (req, res) {
-    var dbcon=db.connect();
+    var dbcon = db.connect();
     const data = req.body;
     var obj;
-    
+
     // var sql = "select * from events where email='"+data.mailId+"'"; expiredate
-    var sql = "select tokendetails from user_transaction WHERE EVENTID ='"+data.eventid+"'";
+    var sql = "select tokendetails from user_transaction WHERE EVENTID ='" + data.eventid + "'";
     console.log(sql);
     dbcon.query(sql, function (err, result, fields) {
         console.log(result);
-      if (err)
-      {
-        console.log("Something Error");
-        console.log(err);
-        obj={
-            "status":"fail",
-            "value":[],
-            "message":"Tech Error"
+        if (err) {
+            console.log("Something Error");
+            console.log(err);
+            obj = {
+                "status": "fail",
+                "value": [],
+                "message": "Tech Error"
             }
 
+            res.send(obj);
+            return false;
+        }
+        var datalength = result.length;
+        if (datalength > 0) {
+            obj = {
+                "status": "success",
+                "value": result
+            }
+        }
+        else {
+            obj = {
+                "status": "success",
+                "value": result,
+                "message": "No Data found"
+            }
+        }
         res.send(obj);
-        return false;
-      } 
-      var datalength = result.length;
-      if(datalength > 0)
-      {
-        obj={
-            "status":"success",
-            "value":result
-            }
-      }
-      else{
-        obj={
-            "status":"success",
-            "value":result,
-            "message":"No Data found"
-            }
-      }
-      res.send(obj);
     });
 }
 exports.userbookedEventsdetails = function (req, res) {
@@ -160,78 +153,70 @@ exports.userbookedEventsdetails = function (req, res) {
     console.log("req.body");
     console.log(req.body);
     var obj;
-    var dbcon=db.connect();
+    var dbcon = db.connect();
 
-    
-    
+
+
     // var sql = "select * from events where email='"+data.mailId+"'"; expiredate
 
- //   var sql = "select tokendetails from user_transaction WHERE USERMAILID ='"+data.usermailid+"'";
-console.log("============SQLLLL========");
-console.log(data);
- var sql = "select tra.ID,tra.USERMAILID,tra.EVENTID,tra.TOKENDETAILS,tra.BOOKEDDATE,tra.STATUS,tra.WINAMOUNT,"+
- "eve.id,eve.EVENTTYPE,eve.SUBEVENTTYPE,eve.EVENTNAME,eve.PRICE,eve.PRIZEAMOUNT,eve.TOTALTOKENS,eve.STARTTOCKEN,eve.EXPIREDATE "+
- "from user_transaction tra inner join events eve on tra.EVENTID=eve.ID where tra.usermailid='"+data.usermailid+"'";
+    //   var sql = "select tokendetails from user_transaction WHERE USERMAILID ='"+data.usermailid+"'";
+    console.log("============SQLLLL========");
+    console.log(data);
+    var sql = "select tra.ID,tra.USERMAILID,tra.EVENTID,tra.TOKENDETAILS,tra.BOOKEDDATE,tra.STATUS,tra.WINAMOUNT," +
+        "eve.id,eve.EVENTTYPE,eve.SUBEVENTTYPE,eve.EVENTNAME,eve.PRICE,eve.PRIZEAMOUNT,eve.TOTALTOKENS,eve.STARTTOCKEN,eve.EXPIREDATE " +
+        "from user_transaction tra inner join events eve on tra.EVENTID=eve.ID where tra.usermailid='" + data.usermailid + "'";
 
     console.log(sql);
     dbcon.query(sql, function (err, result, fields) {
         console.log(result);
-      if (err)
-      {
-        console.log("Something Error");
-        console.log("hhhhh===="+err);
-        return false;
-      } 
-      var datalength = result.length;
-      if(datalength > 0)
-      {
-        obj={
-            "status":"success",
-            "value":result
+        if (err) {
+            obj = {
+                "status": "fail",
+                "message": err.message
             }
-      }
-      else{
-        obj={
-            "status":"fail",
-            "value":result
+            res.send(obj);
+        }
+        // var datalength = result.length;
+        // if (datalength > 0) {
+            obj = {
+                "status": "success",
+                "value": result
             }
-      }
-      db.close();
-      res.send(obj);
+        // }
+        db.close();
+        res.send(obj);
     });
 }
 
 exports.userselectEvents = function (req, res) {
-    var dbcon=db.connect();
+    var dbcon = db.connect();
     const data = req.body;
     var obj;
     var errormsg;
 
-    var sql = "INSERT INTO user_transaction (usermailid, eventid,tokendetails, bookeddate)" + 
-    "VALUES ('"+data.usermailid+"','"+data.eventid+"','"+data.tokendetails+"','"+data.bookeddate+"')";
+    var sql = "INSERT INTO user_transaction (usermailid, eventid,tokendetails, bookeddate)" +
+        "VALUES ('" + data.usermailid + "','" + data.eventid + "','" + data.tokendetails + "','" + data.bookeddate + "')";
     dbcon.query(sql).on('result', function (result) {
-        if(result.affectedRows==1)
-        {
-            obj={
-                "status":"success",
-                "errorMessage":"Booked success"
+        if (result.affectedRows == 1) {
+            obj = {
+                "status": "success",
+                "errorMessage": "Booked success"
             }
         }
     }).on('error', function (err) {
         //console.log(err);
-        if(err.code=='ER_DUP_ENTRY')
-        {
-            let errorindex  = err.sqlMessage.split("for key");
+        if (err.code == 'ER_DUP_ENTRY') {
+            let errorindex = err.sqlMessage.split("for key");
             let errorindex1 = errorindex[1].split("UNIQUE");
             let errorcolumn = errorindex1[0].split("'")
-            errormsg=errorcolumn[1] + "is Exit";
+            errormsg = errorcolumn[1] + "is Exit";
         }
-        else{
+        else {
             errormsg = err.sqlMessage
         }
-        obj={
-            "status":"fail",
-            "errorMessage":errormsg
+        obj = {
+            "status": "fail",
+            "errorMessage": errormsg
         }
     }).on('end', function (data) {
         //Result return to request respons 
